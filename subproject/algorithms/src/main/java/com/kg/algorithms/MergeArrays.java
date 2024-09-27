9cce4ad47099754d98c69af3d22593aa724a6cac
marc.pan <pxy0592@sina.com>
2024-09-27 11:25:16 +0800

    algo: update

--- subproject/algorithms/src/main/java/com/kg/algorithms/MergeArrays.java
+++ subproject/algorithms/src/main/java/com/kg/algorithms/MergeArrays.java
@@ -1,8 +1,6 @@
 package com.kg.algorithms;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class MergeArrays {
     public static void merge(int[] array1, int m, int[] array2, int n) {
@@ -10,7 +8,7 @@ public class MergeArrays {
         Arrays.sort(array2);
 
         if (m == 0) {
-            array1 = array2;
+            System.arraycopy(array2, 0, array1, 0, array1.length);
             return;
         }
 
@@ -18,25 +16,26 @@ public class MergeArrays {
             return;
         }
 
-        List<Integer> arrayList = new ArrayList<Integer>();
-        for (int i=0; i<m; i++) {
-            arrayList.add(array1[i]);
-        }
         for (int i=0; i<n; i++) {
-            arrayList.add(array2[i]);
+            array1[m+i] = array2[i];
         }
 
-        arrayList.removeIf(i -> i ==0);
+        Arrays.sort(array1);
+        print(array1);
+    }
 
-        array1 = arrayList.stream().mapToInt(i -> i).toArray();
+    public static void print(int[] array) {
+        for (int i=0; i<array.length; i++) {
+            System.out.print(array[i]);
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
         int[] nums1 = {1,2,3,0,0,0};
         int[] nums2 = {2,5,6};
-        merge(nums1, nums1.length, nums2, nums2.length);
-        for (int i=0; i<nums1.length; i++) {
-            System.out.print(nums1[i]);
-        }
+        merge(nums1, 3, nums2, 3);
+        print(nums1);
     }
+
 }
